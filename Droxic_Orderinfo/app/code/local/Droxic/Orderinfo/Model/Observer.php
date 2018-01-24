b1308af44ecaac20603e7b12240571661cef8f0d
xdevx32 <angelkukushev333@gmail.com>
2018-01-24 22:36:54 +0200

    Update Observer.php

--- Droxic_Orderinfo/app/code/local/Droxic/Orderinfo/Model/Observer.php
+++ Droxic_Orderinfo/app/code/local/Droxic/Orderinfo/Model/Observer.php
@@ -1,42 +1,7 @@
 <?php
-/**
- * Solvingmagento_OrderExport Observer class
- * 
- * PHP version 5.3
- * 
- * @category  Knm
- * @package   Solvingmagento_OrderExport
- * @author    Oleg Ishenko <oleg.ishenko@solvingmagento.com>
- * @copyright 2013 Oleg Ishenko
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- * @version   GIT: <0.1.0>
- * @link      http://www.solvingmagento.com/
- *
- */
-
-/** Solvingmagento_OrderExport_Model_Observer
- * 
- * @category Knm
- * @package  Solvingmagento_OrderExport
- * 
- * @author   Oleg Ishenko <oleg.ishenko@solvingmagento.com>
- * @license  http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- * @version  Release: <package_version>
- * @link     http://www.solvingmagento.com/
- * 
- * 
- */
 
 class Droxic_Orderinfo_Model_Observer
 {
-    
-    /**
-     * Exports an order after it is placed
-     * 
-     * @param Varien_Event_Observer $observer observer object 
-     * 
-     * @return boolean
-     */
     public function exportOrder(Varien_Event_Observer $observer)
     {
         $order = $observer->getEvent()->getOrder();
@@ -48,14 +13,6 @@ class Droxic_Orderinfo_Model_Observer
         
     }
     
-    /**
-     * Sends an email to the admin after a customer places his first order
-     * online
-     * 
-     * @param Varien_Event_Observer $observer observer object
-     * 
-     * @return boolean
-     */
     public function newCustomer(Varien_Event_Observer $observer)
     {
         $orderIds = $observer->getEvent()->getOrderIds();
