74ed685f59af6488e4e2fb60a6e0eb117a18db0a
xdevx32 <angelkukushev333@gmail.com>
2018-01-24 22:36:20 +0200

    Update Export.php

--- Droxic_Orderinfo/app/code/local/Droxic/Orderinfo/Model/Export.php
+++ Droxic_Orderinfo/app/code/local/Droxic/Orderinfo/Model/Export.php
@@ -1,42 +1,8 @@
 <?php
-/**
- * Solvingmagento_OrderExport Export class
- * 
- * PHP version 5.3
- * 
- * @category  Knm
- * @package   Solvingmagento_OrderExport
- * @author    Oleg Ishenko <oleg.ishenko@solvingmagento.com>
- * @copyright 2013 Oleg Ishenko
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- * @version   GIT: <0.1.0>
- * @link      http://www.solvingmagento.com/
- *
- */
 
-/** Solvingmagento_OrderExport_Model_Export
- * 
- * @category Knm
- * @package  Solvingmagento_OrderExport
- * 
- * @author   Oleg Ishenko <oleg.ishenko@solvingmagento.com>
- * @license  http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- * @version  Release: <package_version>
- * @link     http://www.solvingmagento.com/
- * 
- * 
- */
 class Droxic_Orderinfo_Model_Export
 {
-    
-    /**
-     * Generates an XML file from the order data and places it into
-     * the var/export directory
-     * 
-     * @param Mage_Sales_Model_Order $order order object
-     * 
-     * @return boolean
-     */
+
     public function exportOrder($order) 
     {
         $dirPath = Mage::getBaseDir('var') . DS . 'acho';
