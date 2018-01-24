36c53070159b29b80711dfad48a33e0031d49b92
xdevx32 <angelkukushev333@gmail.com>
2018-01-24 22:40:11 +0200

    Update Observer.php

--- Droxic_Orderinfo/app/code/local/Droxic/Orderinfo/Model/Observer.php
+++ Droxic_Orderinfo/app/code/local/Droxic/Orderinfo/Model/Observer.php
@@ -13,40 +13,4 @@ class Droxic_Orderinfo_Model_Observer
         
     }
     
-    public function newCustomer(Varien_Event_Observer $observer)
-    {
-        $orderIds = $observer->getEvent()->getOrderIds();
-        
-        if (!is_array($orderIds) || (!array_key_exists(0, $orderIds))) {
-            return;
-        }
-        
-        $order = Mage::getModel('sales/order')->load($orderIds[0]);
-        
-        if (!$order->getId()) {
-            return;
-        }
-        
-        if (!$order->getCustomerId()) {
-            //send a message only for registered customers
-            return;
-        }
-        
-        $customer = Mage::getModel('customer/customer')->load($order->getCustomerId());
-        
-        if (!$customer->getId()) {
-            return;
-        }
-        
-        $customerOrders = Mage::getModel('sales/order')
-                ->getCollection()
-                ->addAttributeToFilter('customer_id', $customer->getId());
-        if (count($customerOrders) > 1) {
-            // send a message only after the first order
-            return;
-        }
-        
-        return Mage::getModel('droxic_orderinfo/customer')
-            ->newCustomer($customer, $order);        
-    }
 }
